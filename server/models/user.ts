27daa85fb4f3330f0ee461f1c2efbdd13ef3ba2f
Ricardo Rojas <richi@Ricardos-MacBook-Pro.local>
2020-07-13 19:34:59 -0500

    Feat: Add user validation using JsonSchema

--- server/models/user.ts
+++ server/models/user.ts
@@ -9,6 +9,20 @@ class UserModel extends Model {
   static get tableName() {
     return 'users'
   }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['email', 'password', 'name'],
+
+      properties: {
+        id: { type: 'integer' },
+        email: { type: 'string', minLength: 8, maxLength: 50 },
+        password: { type: 'string', minLength: 6, maxLength: 20 },
+        name: { type: 'string', minLength: 3, maxLength: 50 }
+      }
+    }
+  }
 }
 
 module.exports = UserModel
