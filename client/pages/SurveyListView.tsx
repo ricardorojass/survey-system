6fb7a8ad46b1f6c81d581ba372951300bc4a6d5b
Ricardo Rojas <richi@Ricardos-MacBook-Pro.local>
2020-07-15 18:33:12 -0500

    Feat: Create Survey list view with Survey summary component

---
+++ client/pages/SurveyListView.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { RouteComponentProps } from "react-router"
+
+import SurveySummary from '../components/SurveySummary'
+import Loading from '../components/Loading'
+
+import surveyService from '../services/surveyService'
+import { Survey } from '../types'
+
+interface State {
+  loading?: boolean
+  error?: Error
+  surveys?: Survey[]
+}
+
+interface Props {
+  survey?: string
+}
+
+export default class SurveyListView extends React.Component<RouteComponentProps<Props>, State> {
+  surveyId: string
+
+  constructor(props: RouteComponentProps<Props>) {
+    super(props)
+
+    this.state = { loading: true, error: null, surveys: [] }
+  }
+
+  async componentDidMount() {
+    await this.fetchSurveys()
+  }
+  
+  render() {
+
+    return (
+      <div className="bg-gray-100 h-screen">
+
+        <section className="flex-grow flex justify-center items-center">
+          <div className="mx-auto px-4 sm:px-8 py-2 mt-6">
+            
+            <div className="grid grid-cols-6 gap-4 items-start mt-8 mx-auto px-8">
+              { this.state.surveys.map(survey => 
+                <SurveySummary key={survey.id} survey={survey} />
+              
+              )}
+              
+            </div>
+          </div>
+        </section>
+        
+      </div>
+    )
+  }
+
+  fetchSurveys = async () => {
+    try {
+      const surveys: Survey[] = await surveyService.list()
+      this.setState({ loading: false, surveys })
+    } catch (e) {
+      this.setState({ loading: false, error: e })
+    }
+  }
+}
