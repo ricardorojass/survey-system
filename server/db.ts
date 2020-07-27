a92e3226a9b2e2bb67642097224c2ae2b3a9ddef
Ricardo Rojas <richi@Ricardos-MacBook-Pro.local>
2020-07-27 10:45:57 -0500

    Add logs

--- server/db.ts
+++ server/db.ts
@@ -1,4 +1,7 @@
 import knex from 'knex'
 import config from '../knexfile'
 
+console.log('db', process.env.NODE_ENV);
+console.log('db', config[process.env.NODE_ENV]);
+
 export default knex(config[process.env.NODE_ENV || "development"]);
